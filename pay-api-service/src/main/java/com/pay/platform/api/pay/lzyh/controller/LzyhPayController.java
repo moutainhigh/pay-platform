e9060444848242e43ee896765b7e75c5b2a26220
zhengjintao168 <641101154@qq.com>
2019-07-16 14:25:20 +0800

    Merge branch 'master' of https://github.com/zhengjintao168/pay-platform
    # Conflicts:
    #	.idea/artifacts/pay_api_service_war_exploded.xml
    #	.idea/artifacts/pay_manager_system_war_exploded.xml
    #	.idea/workspace.xml
    #	pay-api-service/pay-api-service.iml
    #	pay-manager-system/pay-manager-system.iml

--- pay-api-service/src/main/java/com/pay/platform/api/pay/lzyh/controller/LzyhPayController.java
+++ pay-api-service/src/main/java/com/pay/platform/api/pay/lzyh/controller/LzyhPayController.java
@@ -137,12 +137,24 @@ public class LzyhPayController extends BaseController {
             AliPushUtil.sendGetQrCodeMessage(nonce, tradeCodeNum, tradeCode.get("secret").toString(), orderModel.getPayFloatAmount());
 
             //线路2：发送socket消息;获取收款码;
-            AppContext.getExecutorService().submit(new Runnable() {
-                @Override
-                public void run() {
-                    appWebSocketService.sendGetQrCodeSocket(nonce, tradeCodeNum, tradeCode.get("secret").toString(), orderModel.getPayFloatAmount());
-                }
-            });
+            appWebSocketService.sendGetQrCodeSocket(nonce, tradeCodeNum, tradeCode.get("secret").toString(), orderModel.getPayFloatAmount());
+
+            //重复推送消息; 避免客户端网络延迟;
+            for (int i = 0; i < 2; i++) {
+
+                AppContext.getExecutorService().submit(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        //线路1：推送阿里消息（双线并行,提高跟app连接通信的成功率）
+                        AliPushUtil.sendGetQrCodeMessage(nonce, tradeCodeNum, tradeCode.get("secret").toString(), orderModel.getPayFloatAmount());
+
+                        //线路2：发送socket消息;获取收款码;
+                        appWebSocketService.sendGetQrCodeSocket(nonce, tradeCodeNum, tradeCode.get("secret").toString(), orderModel.getPayFloatAmount());
+                    }
+                });
+
+            }
 
             json.put("status", "1");
             json.put("msg", "下单成功");
