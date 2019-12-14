1702b176b616824af7a689f9ec6753c1c9eadd87
zhengjintao168 <641101154@qq.com>
2019-12-14 16:36:17 +0800

    Merge branch 'master' of https://github.com/zhengjintao168/pay-platform
    # Conflicts:
    #	.idea/artifacts/pay_api_service_war_exploded.xml
    #	.idea/artifacts/pay_manager_system_war_exploded.xml
    #	.idea/workspace.xml
    #	pay-api-service/pay-api-service.iml
    #	pay-manager-system/pay-manager-system.iml

--- pay-api-service/src/main/java/com/pay/platform/api/pay/charge/controller/PayChargeController.java
+++ pay-api-service/src/main/java/com/pay/platform/api/pay/charge/controller/PayChargeController.java
@@ -55,30 +55,35 @@ public class PayChargeController extends BaseController {
 
         try {
 
-            //获取请求提交数据
-            String text = IOUtils.toString(request.getInputStream(), "utf-8");
-            JSONObject reqJson = new JSONObject(text);
-            String merchantNo = reqJson.getString("merchantNo");
-            String merchantOrderNo = reqJson.getString("merchantOrderNo");
-            String orderAmount = reqJson.getString("orderAmount");
-            String payWay = reqJson.getString("payWay");
-            String notifyUrl = reqJson.getString("notifyUrl");
-            String clientIp = reqJson.getString("clientIp");
-
-            //创建订单
-            String baseUrl = IpUtil.getBaseURL(request);
-            String orderId = payChargeService.createOrderByCharge(merchantNo, merchantOrderNo, orderAmount, payWay, notifyUrl, clientIp, baseUrl);
-            if (StringUtil.isEmpty(orderId)) {
-                json.put("status", "0");
-                json.put("msg", "下单失败");
-                writeJson(response, json.toString());
-                return;
-            }
-
-            json.put("status", "1");
-            json.put("msg", "下单成功");
-            json.put("data", orderId);
+            json.put("status", "0");
+            json.put("msg", "下单失败");
             writeJson(response, json.toString());
+            return;
+
+//            //获取请求提交数据
+//            String text = IOUtils.toString(request.getInputStream(), "utf-8");
+//            JSONObject reqJson = new JSONObject(text);
+//            String merchantNo = reqJson.getString("merchantNo");
+//            String merchantOrderNo = reqJson.getString("merchantOrderNo");
+//            String orderAmount = reqJson.getString("orderAmount");
+//            String payWay = reqJson.getString("payWay");
+//            String notifyUrl = reqJson.getString("notifyUrl");
+//            String clientIp = reqJson.getString("clientIp");
+//
+//            //创建订单
+//            String baseUrl = IpUtil.getBaseURL(request);
+//            String orderId = payChargeService.createOrderByCharge(merchantNo, merchantOrderNo, orderAmount, payWay, notifyUrl, clientIp, baseUrl);
+//            if (StringUtil.isEmpty(orderId)) {
+//                json.put("status", "0");
+//                json.put("msg", "下单失败");
+//                writeJson(response, json.toString());
+//                return;
+//            }
+
+//            json.put("status", "1");
+//            json.put("msg", "下单成功");
+//            json.put("data", orderId);
+//            writeJson(response, json.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,38 +109,42 @@ public class PayChargeController extends BaseController {
 
         try {
 
-            String secretData = IOUtils.toString(request.getInputStream(), "utf-8");
-            String sign = URLDecoder.decode(new JSONObject(secretData).getString("sign"), "UTF-8");
-
-            //解密获取报文
-            AESOperator aes = new AESOperator(PayUtil.AES_SECRET);
-            String text = aes.decrypt(sign);
-            getCurrentLogger().info("收到支付回调请求：" + text);
-            JSONObject reqJson = new JSONObject(text);
-
-            if (200 == reqJson.getInt("resultCode")) {
-                JSONObject data = reqJson.getJSONObject("data");
-
-                //支付成功
-                if ("SUCCESS".equalsIgnoreCase(data.getString("status"))) {
-                    String platformOrderNo = data.getString("merchantOrderNo");
-                    String payTime = data.getString("payTime");
-                    String channelActuatAmount = data.getString("amount");
-
-                    //相关业务处理：更新订单状态等
-                    boolean flag = orderServicel.paySuccessBusinessHandle(platformOrderNo, null, payTime);
-
-                    //推送支付回调给商家
-                    if (flag) {
-                        merchantNotifyService.pushPaySuccessInfoByRetry(platformOrderNo);
-                    }
-
-                }
-
-            }
-
             response.getWriter().write("ok");
             response.getWriter().flush();
+            return;
+
+//            String secretData = IOUtils.toString(request.getInputStream(), "utf-8");
+//            String sign = URLDecoder.decode(new JSONObject(secretData).getString("sign"), "UTF-8");
+//
+//            //解密获取报文
+//            AESOperator aes = new AESOperator(PayUtil.AES_SECRET);
+//            String text = aes.decrypt(sign);
+//            getCurrentLogger().info("收到支付回调请求：" + text);
+//            JSONObject reqJson = new JSONObject(text);
+
+//            if (200 == reqJson.getInt("resultCode")) {
+//                JSONObject data = reqJson.getJSONObject("data");
+
+//                //支付成功
+//                if ("SUCCESS".equalsIgnoreCase(data.getString("status"))) {
+//                    String platformOrderNo = data.getString("merchantOrderNo");
+//                    String payTime = data.getString("payTime");
+//                    String channelActuatAmount = data.getString("amount");
+//
+//                    //相关业务处理：更新订单状态等
+//                    boolean flag = orderServicel.paySuccessBusinessHandle(platformOrderNo, null, payTime);
+//
+//                    //推送支付回调给商家
+//                    if (flag) {
+//                        merchantNotifyService.pushPaySuccessInfoByRetry(platformOrderNo);
+//                    }
+//
+//                }
+
+//            }
+
+//            response.getWriter().write("ok");
+//            response.getWriter().flush();
 
         } catch (Exception e) {
             e.printStackTrace();
