650615c088f2037cf0b05837842037eae0122612
zhengjintao168 <641101154@qq.com>
2019-12-29 18:34:36 +0800

    Merge branch 'master' of https://github.com/zhengjintao168/pay-platform
    # Conflicts:
    #	.idea/artifacts/pay_api_service_war_exploded.xml
    #	.idea/artifacts/pay_manager_system_war_exploded.xml
    #	.idea/workspace.xml
    #	pay-api-service/pay-api-service.iml
    #	pay-manager-system/pay-manager-system.iml

--- pay-api-service/src/main/java/com/pay/platform/api/base/controller/TestController.java
+++ pay-api-service/src/main/java/com/pay/platform/api/base/controller/TestController.java
@@ -9,6 +9,7 @@ import com.pay.platform.common.websocket.service.AppWebSocketService;
 import com.pay.platform.security.util.AppSignUtil;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.UUID;
 
+@Controller
 public class TestController  extends BaseController {
 
     @Autowired
